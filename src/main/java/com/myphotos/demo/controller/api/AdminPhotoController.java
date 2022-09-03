7f76f09a13b56313d0a9ce7f7f6cea5e5e1d5e74
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-09-03 11:41:42 +0200

    Add update method

--- src/main/java/com/myphotos/demo/controller/api/AdminPhotoController.java
+++ src/main/java/com/myphotos/demo/controller/api/AdminPhotoController.java
@@ -60,4 +60,21 @@ public class AdminPhotoController
 		list.add(photo);
 		return photo;
 	}
+	
+	@RequestMapping(value = "/admin/api/photos/{id}", method = RequestMethod.PUT)
+	public Photo update(@PathVariable int id, @RequestBody Photo photo)
+	{
+		Optional<Photo> foundPhoto = list.stream()
+				.filter(item->item.getId() == id)
+				.findFirst();
+		
+		if (foundPhoto.isEmpty())
+		{
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Item not found");
+		}
+		
+		foundPhoto.get().setUrl(photo.getUrl());
+		
+		return foundPhoto.get();
+	}
 }
